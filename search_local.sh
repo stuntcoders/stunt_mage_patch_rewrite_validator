569c6358bce7e1a11a20bf7d074207aafa45905c
Radisa Tucovic <radisa@stuntcoders.com>
2017-10-12 18:55:42 +0200

    Add script

---
+++ search_local.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+modified_files=$(git ls-files -m);
+num_modified=$(wc -l <<< "$modified_files");
+echo "Modified "$num_modified;
+
+RED='\033[0;31m';
+GREEN='\033[0;32m';
+DEFAULT='\033[0m';
+COLOR=$DEFAULT;
+
+while read -r line ; do
+	COLOR=$GREEN;
+	
+	to_check=$(echo $line | sed "s/app\/code\/core/app\/code\/local/");
+	if [[ -f $to_check ]];
+	then
+		COLOR=$RED;	
+	fi
+
+	to_check=$(echo $line | sed "s/app\/code\/core/app\/code\/community/");
+	if [[ -f $to_check ]];
+	then
+		COLOR=$RED;
+	fi
+
+	echo -e "${COLOR}$line"
+done < <(grep core <<< "$modified_files")
+
+while read -r line ; do
+	COLOR=$GREEN;
+
+	to_check=$(echo $line | sed 's/^.*template/*/');
+	if [[ $(find . -wholename "*$to_check" | wc -l) -gt 1 ]];
+	then
+		COLOR=$RED;
+	fi
+
+	echo -e "${COLOR}$line"
+done < <(grep template <<< "$modified_files")
+
+echo -e "${DEFAULT}Additional files: ";
+additional=$(grep -v core <<< "$modified_files");
+additional=$(grep -v template <<< "$additional");
+echo -e "${RED}$additional";
